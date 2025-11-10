7022cf0773bd4e4cda0d69dd1007e5ad6e180cb0
Flávio Antônio <flavusantonio@gmail.com>
2025-11-10 20:30:26 -0300

    Foooter alterado

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-gray-800 text-blue-400 text-center p-4 mt-8">
-      <p>&copy; {new Date().getFullYear()} Flavusdevoper. Todos os direitos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} Copyright  Flavusdevoper. Todos os direitos reservados.</p>
     </footer>
   );
 }
